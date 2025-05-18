edf5255e721a3018095644ca9fae0993a54975f3
Elias Abi Chakra <eliasabichacra89@gmail.com>
2025-05-18 10:52:03 +0300

    update vite

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
 import { defineConfig } from 'vite'
-
 import tailwindcss from '@tailwindcss/vite'
-
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),
-  tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+  server: {
+    host: true, // Allows access from external IPs / domains
+    allowedHosts: ['ec2-51-21-199-13.eu-north-1.compute.amazonaws.com'], // Allow your EC2 DNS
+  },
 })
