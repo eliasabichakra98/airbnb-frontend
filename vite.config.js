6a59f6829a5367b334ce9f79c55c4197c674c1be
Elias Abi Chakra <eliasabichacra89@gmail.com>
2025-05-19 19:48:38 +0300

    update cors

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,7 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    host: true, // Allows access from external IPs / domains
-    allowedHosts: ['ec2-51-21-199-13.eu-north-1.compute.amazonaws.com'], // Allow your EC2 DNS
+    host: true,
+    allowedHosts: 'all',  // Allow all hosts
   },
 })
