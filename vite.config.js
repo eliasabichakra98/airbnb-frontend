cb8b953d330315dd51906e1f4f3e098a591bac15
Elias Abi Chakra <eliasabichacra89@gmail.com>
2025-05-21 19:39:33 +0300

    running cicd

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,6 @@ export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
     host: true, // Allows access from external IPs / domains
-    allowedHosts: ['ec2-16-171-144-210.eu-north-1.compute.amazonaws.com'], // Allow your EC2 DNS
+    allowedHosts: ['ec2-16-171-29-225.eu-north-1.compute.amazonaws.com'], // Allow your EC2 DNS
   },
 })
